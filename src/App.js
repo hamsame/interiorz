218614914e98a4ed53a486be6cd5db00894c3ca4
Hamsa Elmi <73555504+hamsame@users.noreply.github.com>
2022-01-04 00:24:35 +0000

    Created Navbar Feature (#2)
    * Created navbar and added styling using sass
    
    * Added padding,margin and colour to navbar and made it responsive

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import NotFound from './pages/NotFound'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route index element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
         <Route exact path='*' element={<NotFound />} />
       </Routes>
     </Router>
