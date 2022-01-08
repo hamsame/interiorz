d5cf0e8751fe0e9b2fd1b2db13354a30da931d7a
hamsame <hamsaaelmi@gmail.com>
2022-01-08 16:17:45 +0000

    Stated creating homepage. Made hero and 'our process' sections so far styled using scss.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,61 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
+    <main>
+      <section className='hero'>
+        <div className='hero-content'>
+          <h1>Professional Interior Design</h1>
+          <p>Professional Interior Design at Affordable Prices</p>
+        </div>
+      </section>
+
+      <section className='process'>
+        <h2>Our Process</h2>
+        <article>
+          <span>
+            <h3>1) Lorem ipsum dolor sit.</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto
+              numquam provident exercitationem ab ullam esse alias commodi, enim
+              facilis maxime repudiandae omnis natus nemo voluptatibus!
+            </p>
+          </span>
+          <img
+            src='https://images.pexels.com/photos/6457544/pexels-photo-6457544.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            alt=''
+          />
+        </article>
+        <article>
+          <span>
+            <h3>2) Lorem ipsum dolor sit.</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+              doloremque fugit molestias tempora quia modi labore delectus,
+              voluptates unde dolorem beatae, ipsa veniam sit illo atque aut.
+              Laborum, recusandae libero.
+            </p>
+          </span>
+          <img
+            src='https://images.pexels.com/photos/6457544/pexels-photo-6457544.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            alt=''
+          />
+        </article>
+        <article>
+          <span>
+            <h3>3) Lorem ipsum dolor sit.</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae,
+              accusantium! Modi error numquam nisi ab doloremque deleniti cumque
+              provident sint.
+            </p>
+          </span>
+          <img
+            src='https://images.pexels.com/photos/6457544/pexels-photo-6457544.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            alt=''
+          />
+        </article>
+      </section>
+    </main>
   )
 }
 
