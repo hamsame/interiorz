8a25dae39197695327ab4299e7f963ad7d2d8e18
Hamsa Elmi <73555504+hamsame@users.noreply.github.com>
2022-01-14 19:00:30 +0000

    Merging changes into main branch (#7)
    * Created new react app and added routing
    
    * Created Navbar Feature (#2)
    
    * Created navbar and added styling using sass
    
    * Added padding,margin and colour to navbar and made it responsive

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { NavLink as PageLink } from 'react-router-dom'
+
+const Navbar = () => {
+  const [icon, setIcon] = useState('☰')
+  const showNav = () => {
+    const nav = document.querySelector('nav')
+    const ul = nav.children[2]
+    icon === '☰' ? setIcon('X') : setIcon('☰')
+    console.log(ul)
+    ul.classList.toggle('shown')
+  }
+
+  return (
+    <nav>
+      <h1>
+        <PageLink to='/'>Interiorz</PageLink>
+      </h1>
+      <span className='hamburger' onClick={() => showNav()}>
+        {icon}
+      </span>
+      <ul>
+        <li>
+          <PageLink to='/'>Home</PageLink>
+        </li>
+        <li>
+          <PageLink to='about'>About</PageLink>
+        </li>
+        <li>
+          <PageLink to='contact'>Contact</PageLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
