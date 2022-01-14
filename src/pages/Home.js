e5d667167bb1c0edbea895a41fd3be17bbdbf29a
hamsame <hamsaaelmi@gmail.com>
2022-01-14 21:26:50 +0000

    Changed images, added some website copy, and edited flexbox layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <main>
+    <main className='home'>
       <section className='hero'>
         <div className='hero-content'>
-          <h1>Professional Interior Design</h1>
+          <h1>Interiorz</h1>
           <p>Professional Interior Design at Affordable Prices</p>
         </div>
       </section>
@@ -14,12 +14,7 @@ const Home = () => {
         <h2>Our Process</h2>
         <article>
           <span>
-            <h3>1) Lorem ipsum dolor sit.</h3>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto
-              numquam provident exercitationem ab ullam esse alias commodi, enim
-              facilis maxime repudiandae omnis natus nemo voluptatibus!
-            </p>
+            <h3> Define Your Project</h3>
           </span>
           <img
             src='https://images.pexels.com/photos/6457544/pexels-photo-6457544.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
@@ -28,30 +23,19 @@ const Home = () => {
         </article>
         <article>
           <span>
-            <h3>2) Lorem ipsum dolor sit.</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-              doloremque fugit molestias tempora quia modi labore delectus,
-              voluptates unde dolorem beatae, ipsa veniam sit illo atque aut.
-              Laborum, recusandae libero.
-            </p>
+            <h3>Project Consultation</h3>
           </span>
           <img
-            src='https://images.pexels.com/photos/6457544/pexels-photo-6457544.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            src='https://images.pexels.com/photos/7439123/pexels-photo-7439123.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
             alt=''
           />
         </article>
         <article>
           <span>
-            <h3>3) Lorem ipsum dolor sit.</h3>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae,
-              accusantium! Modi error numquam nisi ab doloremque deleniti cumque
-              provident sint.
-            </p>
+            <h3>Deliver Design & Follow-Up</h3>
           </span>
           <img
-            src='https://images.pexels.com/photos/6457544/pexels-photo-6457544.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            src='https://images.pexels.com/photos/1181623/pexels-photo-1181623.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
             alt=''
           />
         </article>
