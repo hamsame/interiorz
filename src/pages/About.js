a24f95ef97caf18167dd17137e39d3972e76425f
hamsame <hamsaaelmi@gmail.com>
2022-01-16 21:18:03 +0000

    Created and styled about us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,41 @@
 import React from 'react'
+import { serviceDetails } from '../components/ServiceDetails'
 
 const About = () => {
   return (
-    <div>
-      <h1>About page</h1>
-    </div>
+    <main className='about'>
+      <h1>About Us</h1>
+      <section>
+        <article>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+            tenetur nostrum minus commodi hic nihil necessitatibus id,
+            exercitationem ex fuga fugiat quasi expedita in itaque! Porro
+            aliquam iusto voluptates, a minus natus, labore sapiente quasi ipsam
+            quae nesciunt dolor ea omnis maxime nihil? Quasi ea dolore adipisci
+            in id deleniti, rem ducimus, impedit ullam excepturi omnis nihil
+            beatae, sunt rerum quos atque cumque et porro. Nesciunt vitae
+            molestiae quos dolorum in beatae maxime, exercitationem cumque.
+            Consectetur accusantium non recusandae, est aut sequi corrupti
+            explicabo quas molestiae atque illo nisi eos dignissimos unde quidem
+            nihil enim totam, impedit eum doloribus similique!
+          </p>
+          <img
+            src='https://images.pexels.com/photos/7413888/pexels-photo-7413888.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            alt='Our founders'
+          />
+        </article>
+      </section>
+
+      <section className='about-services'>
+        <h2>Our services include:</h2>
+        <ul>
+          {serviceDetails.map((info, index) => {
+            return <li key={index}>{info.service}</li>
+          })}
+        </ul>
+      </section>
+    </main>
   )
 }
 
