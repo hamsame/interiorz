05cb564ddf0dca113de66504bed240bf6dfc505f
hamsame <hamsaaelmi@gmail.com>
2022-01-24 23:33:34 +0000

    Installed contenful package using npm, created blogs index page and rendered fetched blog titles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import NotFound from './pages/NotFound'
 import Navbar from './components/Navbar'
+import Blog from './pages/blog'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path='about' element={<About />} />
         <Route path='contact' element={<Contact />} />
+        <Route path='blog' element={<Blog />} />
         <Route exact path='*' element={<NotFound />} />
       </Routes>
     </Router>
