7c014a7497cbae6df25cfff1d3b8ff055fbd2637
hamsame <hamsaaelmi@gmail.com>
2022-02-03 16:53:39 +0000

    UAdded some copy for the footer and added styling.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <footer>
-      <h2>Lorem, ipsum dolor.</h2>
+      <section>
+        <article className='footer-f'>
+          <h2>Interiorz</h2>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </article>
+        <hr />
+        <article className='footer-f'>
+          <div>
+            <h3>Join Our Email List</h3>
+            <p>Receive weekly emails about how to design, offers and more!</p>
+            <input type='email' name='email' id='email' />
+            &nbsp;
+            <button type='submit' className='submit-btn'>
+              SUBMIT
+            </button>
+          </div>
+
+          <ul>
+            <h3>Pages</h3>
+            <br />
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/blog'>Blog</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          </ul>
+        </article>
+      </section>
     </footer>
   )
 }
